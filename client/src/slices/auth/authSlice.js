fc5f1da01a3189a9db11b62e42d04bd4e07676b5
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-21 16:27:28 +0000

    Jul 21, 2023, 5:27 PM

--- client/src/slices/auth/authSlice.js
+++ client/src/slices/auth/authSlice.js
@@ -2,9 +2,9 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authService";
 
 // Get user from localStorage
-//const user = JSON.parse(localStorage.getItem("user"));
+const user = JSON.parse(localStorage.getItem("user"));
 const initialState = {
-  user: [],
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
